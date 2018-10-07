d2eb0201c5844692aa5f115dde16a09acf6d8915
Faris Adlan A <farisdx66@gmail.com>
2018-10-07 21:28:47 +0700

    add Relay/Selonoid

---
+++ MFRC522/RelaySelo.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.LOW)
+
+time.sleep(0.25)
+
+GPIO.output(17, GPIO.HIGH)
+GPIO.cleanup()
