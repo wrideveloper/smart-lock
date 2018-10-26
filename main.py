5b70c85dee00efb43a2add6dba622eb71f38d965
masaditya <aeka489@gmail.com>
2018-10-26 15:44:01 +0700

    nyoba sek yo

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 import datetime
 import requests
 import RPi.GPIO as GPIO
-from  MFRC522 import  MFRC522
+# from MFRC522 import MFRC522
+import MFRC522
+
 import time
 import sys
-from MFRC522.Read import detected
+# from MFRC522.Read import detected
 
 
 app = Flask(__name__)
@@ -81,13 +83,15 @@ class PeriksaUid(Resource):
 
 """Fungsi Untuk Validasi"""
 
+
 def cek_status(uid):
     otomat_validation = LogActivity.query.filter_by(
-        uid = uid, user_in = None
+        uid=uid, user_in=None
     ).first()
 
     if otomat_validation is None:
         user_masuk_keluar(uid)
+
 
 def user_masuk_keluar(uid):
 
@@ -97,7 +101,8 @@ def user_masuk_keluar(uid):
 
     if user_validation:
 
-        api_get = requests.get('http://127.0.0.1:5000/smartlock/wri/api/v1/{}/'.format(uid))
+        api_get = requests.get(
+            'http://127.0.0.1:5000/smartlock/wri/api/v1/{}/'.format(uid))
 
         if api_get.status_code == 200:
 
@@ -106,7 +111,44 @@ def user_masuk_keluar(uid):
         else:
             abort()
 
+
 """Fungsi Read RFID"""
+
+
+def reading(self, continue_reading):
+    while continue_reading:
+        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+        if status == MIFAREReader.MI_OK:
+            print "Card detected"
+            (status, uid) = MIFAREReader.MFRC522_Anticoll()
+            autentikasi(status, uid)
+
+
+def autentikasi(self, status, uid):
+    print "card read uid: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3])
+    uuid = "%s%s%s%s" % (uid[0], uid[1], uid[2], uid[3])
+
+    key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+
+    MIFAREReader.MFRC522_SelectTag(uid)
+    status = MIFAREReader.MFRC522_Auth(
+        MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+    # Check if authenticated
+    if status == MIFAREReader.MI_OK:
+        MIFAREReader.MFRC522_Read(8)
+        client.cek_status(str(uuid))
+        MIFAREReader.MFRC522_StopCrypto1()
+        end_read()
+    else:
+        print "Authentication error"
+
+
+def end_read(signal, frame):
+    # global continue_reading
+    print "Ctrl+C captured, ending read."
+    #continue_reading = False
+    GPIO.cleanup()
 
 
 """ API Route """
@@ -120,6 +162,10 @@ api.add_resource(PeriksaUid, *routes)
 
 
 if __name__ == '__main__':
-    #main()
+    # main()
     app.run(debug=True)
-    detected()
+    signal.signal(signal.SIGINT, end_read)
+    MIFAREReader = MFRC522.MFRC522()
+    print "Welcome to the Workshop Riset Infromatika"
+    global continue_reading = True
+    reading(continue_reading)
