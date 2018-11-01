abe29dbf67d0e0f8528461fb6c01f1a5b8c9fd2e
yafiabiyyu <luckyveri3@gmail.com>
2018-11-02 02:07:22 +0700

    test SQLAlchemy

--- main.py
+++ main.py
@@ -66,30 +66,39 @@ class LogActivity(db.Model):
 
 
 class UserWeb(UserMixin,db.Model):
+
     __tablename__ = 'pengguna'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(10))
-    password_has = db.Column(db.String(255))
+    username = db.Column(db.String(60), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
 
     @property
     def password(self):
-        raise AttributeError('Passeord is not a readable attribute')
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
 
-    def verify_password(self,password):
-        return check_password_hash(self.password_has, password)
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<UserWeb : {}>'.format(self.username)
+        return '<UserWeb: {}>'.format(self.username)
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return UserWeb.query.get(user_id)
-
-
-
-class UserWeb(db.Model):
-    __tablename__ = 'user_db'
-    id = db.Column()
+# Set up user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return UserWeb.query.get(int(user_id))
 
 
 class PeriksaUid(Resource):
