ad7ec08ba0c7c8664f04c622e70a10c292850556
yafiabiyyu <luckyveri3@gmail.com>
2018-11-21 19:22:02 +0700

    hapus file

--- admin/form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
